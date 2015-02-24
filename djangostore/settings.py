22e76fcfb6fc124ca616691c736937a537a8cdda
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2015-02-24 14:00:27 -0500

    layout + settings

--- djangostore/settings.py
+++ djangostore/settings.py
@@ -85,3 +85,13 @@ STATIC_URL = '/static/'
 
 # Location of templates
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "static", "templates"), )
+
+if DEBUG:
+    MEDIA_URL        = '/media/'
+    STATIC_ROOT      = os.path.join(BASE_DIR, "static", "static-only")
+    MEDIA_ROOT       = os.path.join(BASE_DIR, "static", "media")
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static", "static"), )
+
+
+
+
--- djangostore/urls.py
+++ djangostore/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+	urlpatterns == static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns == static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
